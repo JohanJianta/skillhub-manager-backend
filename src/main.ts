fff994a73eaa1f83d0b472af8461df92040fc8dc
JohanJianta <johanreinaldojianta@gmail.com>
2025-11-23 16:48:50 +0800

    add enableCors and change port to 3001

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,11 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://192.168.1.7:3000'],
+    credentials: true,
+  });
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
